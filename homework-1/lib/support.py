6ed0c633132901feaca3dbcffac804b0be35c81c
neudachina <eva.neudachina@gmail.com>
2023-04-30 19:35:55 +0300

    udpserver code made shorter

--- homework-1/lib/support.py
+++ homework-1/lib/support.py
@@ -1,9 +1,25 @@
 import os
 
-localIP = "0.0.0.0"
-multicastIP = os.getenv("multicastIP", default="224.0.0.0")
 bufferSize = 1024
 formats = 7
+
+defaultMessage = b'get_time'
+unknownSerializationTypeMessage = b'unknown serialization type\n'
+unknownRequestTypeMessage = b'unknown request type\n'
+
+getRequest = b'get_result'
+
+serverIP = 'server'
+nativeIP = 'native'
+xmlIP = 'xml'
+jsonIP = 'json'
+yamlIP = 'yaml'
+messagepackIP = 'messagepack'
+protobufIP = 'protobuf'
+avroIP = 'avro'
+
+localIP = "0.0.0.0"
+multicastIP = os.getenv("multicastIP", default="224.0.0.0")
 
 serverPort = 2000
 nativePort = 2001
@@ -17,28 +33,16 @@ xmlPort = 2008
 
 clientPort = 3000
 
-defaultMessage = b'get_time'
-unknownSerializationTypeMessage = b'unknown serialization type\n'
-unknownRequestTypeMessage = b'unknown request type\n'
-
-getRequest = b'get_result'
-allRequest = b'ALL'
-nativeRequest = b'NATIVE'
-xmlRequest = b'XML'
-jsonRequest = b'JSON'
-yamlRequest = b'YAML'
-messagepackRequest = b'MESSAGEPACK'
-protobufRequest = b'PROTOBUF'
-avroRequest = b'AVRO'
-
-serverIP = 'server'
-nativeIP = 'native'
-xmlIP = 'xml'
-jsonIP = 'json'
-yamlIP = 'yaml'
-messagepackIP = 'messagepack'
-protobufIP = 'protobuf'
-avroIP = 'avro'
+addresses = {
+    b'ALL': (multicastIP, multicastPort),
+    b'NATIVE': (nativeIP, nativePort),
+    b'XML': (xmlIP, xmlPort),
+    b'JSON': (jsonIP, jsonPort),
+    b'YAML': (yamlIP, yamlPort),
+    b'MESSAGEPACK': (messagepackIP, messagepackPort),
+    b'PROTOBUF': (protobufIP, protobufPort),
+    b'AVRO': (avroIP, avroPort)
+}
 
 avroDefinition = """{
     "namespace": "object.avro",
--- homework-1/lib/udp-server/udp-server.py
+++ homework-1/lib/udp-server/udp-server.py
@@ -13,43 +13,21 @@ socket.bind((support.localIP, support.serverPort))
 
 while True:
     message, address = socket.recvfrom(support.bufferSize)
-    message = message.strip().split(b' ')
-    if message[0] == support.getRequest:
-        if message[1] == support.nativeRequest:
-            addr = (support.nativeIP, support.nativePort)
+    request, serialization_type = message.strip().split(b' ')[:2]
+    if request == support.getRequest:
+        if serialization_type in list(support.addresses.keys()):
+            addr = support.addresses[serialization_type]
             socket.sendto(support.defaultMessage, addr)
-        elif message[1] == support.xmlRequest:
-            addr = (support.xmlIP, support.xmlPort)
-            socket.sendto(support.defaultMessage, addr)
-        elif message[1] == support.jsonRequest:
-            addr = (support.jsonIP, support.jsonPort)
-            socket.sendto(support.defaultMessage, addr)
-        elif message[1] == support.yamlRequest:
-            addr = (support.yamlIP, support.yamlPort)
-            socket.sendto(support.defaultMessage, addr)
-        elif message[1] == support.messagepackRequest:
-            addr = (support.messagepackIP, support.messagepackPort)
-            socket.sendto(support.defaultMessage, addr)
-        elif message[1] == support.protobufRequest:
-            addr = (support.protobufIP, support.protobufPort)
-            socket.sendto(support.defaultMessage, addr)
-        elif message[1] == support.avroRequest:
-            addr = (support.avroIP, support.avroPort)
-            socket.sendto(support.defaultMessage, addr)
-        elif message[1] == support.allRequest:
-            addr = (support.multicastIP, support.multicastPort)
-            socket.sendto(support.defaultMessage, addr)
+            if serialization_type == support.allRequest:
+                for _ in range(support.formats):
+                    answer, _ = socket.recvfrom(support.bufferSize)
+                    socket.sendto(answer, address)
+                continue
+            else:
+                answer = socket.recv(support.bufferSize)
         else:
             answer = support.unknownSerializationTypeMessage
-            socket.sendto(answer, address)
     else:
         answer = support.unknownRequestTypeMessage
-        socket.sendto(answer, address)
 
-    if message[1] == support.allRequest:
-        for _ in range(support.formats):
-            answer, _ = socket.recvfrom(support.bufferSize)
-            socket.sendto(answer, address)
-    else:
-        answer = socket.recv(support.bufferSize)
-        socket.sendto(answer, address)
+    socket.sendto(answer, address)
